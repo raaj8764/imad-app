6395fff9ccf843ec8fd68640cfd16de7587646c1
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-07 01:33:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,13 +85,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createHtml(Articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 
 app.get('/ui/style.css', function (req, res) {
