fb005befd4f795c48a469c1799c25b04628642f8
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-12 01:32:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,5 @@
 var element=document.getElementById('main-text');
 element.innerHTML='SHIVA';
 var img=document.getElementById('img');
-img.onclick = function(){
-  img.style.marginleft= 100px; 
-};
+
+
