deed655e0f7f91f7a610c0bbde6132657a36f275
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-12 01:44:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ button.onclick=function(){
     
     
     counter=counter+1;
-    var span=document.getElementById('ClickNo');
+    var span=document.getElementById('ClickNO');
     span.innerHTML=counter.toString();
     
 }
