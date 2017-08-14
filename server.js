a395dff089be2de4f85c41dcdc217e8fbbc78a83
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-14 16:39:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,14 @@ app.get('/counter',function (req, res) {
     res.send(counter.toString());
 });
 
+var names=[];
+app.get('/submitname/:name',function(req,res){
+    var name=res.params.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createHtml(Articles[articleName]));
