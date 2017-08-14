0f17113b44e4ffb73799358fa4b6705ff2e2de43
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-14 23:14:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,50 +1,44 @@
-var button=document.getElementById('counter');
-document.getElementById("button").onclick=function(){
+var button = document.getElementById('counter');
+//var counter = 0;
+button.onclick = function (){
     
-    var request=new XMLHttpRequest();
-    
-    
-   request.onreadystatechange = function(){
-
-        //take some action
-        if(request.readyState === 4 && request.status === 200){
-          var counter =  request.responseText;
-         var span = document.getElementById("count");
-         span.innerHTML = counter.toString(); 
-        }
-
-    //Not done yet
-};
-//make a request
-    request.open('GET','http://revanthr47.imad.hasura-app.io/counter', true);
-    request.send(null);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+           var counter = request.responseText;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+}
+},
+request.open('GET', 'http://revanthr47.imad.hasura-app.io/counter', true);
+request.send(null);
 };
 
-//submit name
-var nameinput=document.getElementById('name');
-name=nameinput.value;
-var submit=document.getElementById('submit_btn');
-submit.onclick=function(){
-   
-    
-   request.onreadystatechange = function(){
 
-        //take some action
-        if(request.readyState === 4){
-       if(request.status === 200){
-          var names=request.responseText;
-          names=JSON.parse(names);
-          var list='';
-          for(var i=0;i<names.lenght;i++)
-          {
-          list+='<li>'+ names[i] +'</li>';
-          }
-        var ul=document.getElementById('namelist');
-        ul.innerHTML=list;
-        }
-        }
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+     var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+           var names = request.responseText;
+   names = JSON.parse(names);
+   var list = '';
+   for(var i=0; i< names.length; i++){
+       
+       list += '<li>' + names[i] + '</li>';
+   }
+    
+    var ul =document.getElementById('name list');
+    ul.innerHTML = list;
+}
+}
 };
-    request.open('GET','http://revanthr47.imad.hasura-app.io/submitname?name=' +name, true);
-    request.send(null);
-}; 
-  
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+request.open('GET', 'http://revanthr47.imad.hasura-app.io/submit-name?name=' + name, true);
+request.send(null);
+};
