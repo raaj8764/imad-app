5a090349335dc26ce07aa594c405d85989c135a6
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-14 23:16:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 var button = document.getElementById('counter');
 //var counter = 0;
-button.onclick = function (){
+document.getElementById("button").onclick = function (){
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
