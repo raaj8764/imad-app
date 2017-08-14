b368d9939bfcb2f6d7cd86803ac7220f2c55b153
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-14 23:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function (){
        list += '<li>' + names[i] + '</li>';
    }
     
-    var ul =document.getElementById('name list');
+    var ul =document.getElementById('namelist');
     ul.innerHTML = list;
 }
 }
