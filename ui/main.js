b33ecff753c453653aca8909027e5c180dec3548
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-14 12:52:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,19 @@ document.getElementById("button").onclick=function(){
     var request=new XMLHttpRequest();
     
     
-    Request.onreadystatechange=function(){
-    if(request.readystate===XMLHttpRequest.DONE){
-        if(request.status===200){
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
+   request.onreadystatechange = function(){
+
+        //take some action
+        if(request.readyState === 4 && request.status === 200){
+          var counter =  request.responseText;
+         var span = document.getElementById("count");
+         span.innerHTML = counter.toString(); 
         }
-    }
-    };
-    request.open('GET','http://revanthr47.imad.hasura-app.io/counter',true);
+
+    //Not done yet
+};
+//make a request
+    request.open('GET','http://revanthr47.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
