ed14103ef601c9c2aa780a277aa2c99427eedb13
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-27 20:26:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,44 +52,7 @@ var Articles={
 `},
 };
 
-function createHtml(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    
-    var HtmlTemplate=`
-                    <html>
-                    <head>
-                    	<title>
-                    	    ${title}
-                    	</title>
-                    	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-                    	<link href="/ui/style.css" rel="stylesheet" />
-                    </head>
-                        
-                    <body>
-                        		<div class="container">
-                            		<div>
-                            			<a href="/">HOME</a>
 
-                            		</div>
-                            		   <hr/>
-                            			    <h3> 
-                            		    	${heading}
-                            		    	</h3>
-                                			<div>
-                                			${date}
-                                			</div>
-                                			<div>
-                            		    	${content}
-                            		    	</div>
-                        			</div>
-                    </body>
-                    </html>
-                    `;
-                    return HtmlTemplate;
-}
 
 var counter=0;
 app.get('/counter',function (req, res) {
