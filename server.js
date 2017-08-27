4d9edec601629471d5d7dccaa950f77c0a0adbaa
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-27 19:59:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,6 +118,7 @@ pool.query('SELECT * FROM test', function (err,result){
     }
 });
 });
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
     res.send(createHtml(Articles[articleName]));
