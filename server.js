f03c47010e5bb7408fa55c2e904abc5b23517eee
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-27 19:58:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,146 +1,146 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var config = {
-    user: 'revanthr47',
-    database: 'revanth',
-    host: 'db.imad.hasura-app.io',
-    port: '5432',
+var Pool =require('pg').Pool;
+var config ={
+    host:"db.imad.hasura-app.io",
+    user:"revanthr47",
+    database:"revanthr47",
+    port:"5432",
     password: process.env.DB_PASSWORD
 };
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-    'article-one' : {
-    title : 'Article one i kanth',
-    heading : 'Article one',
-    date : 'sep14,2017',
-    content : `
-    <p>
-    This is content of my first article.  This is content of my first article.  This is content of my first article.
-      This is content of my first article.  This is content of my first article.
-      </p>
-      <p>
-        This is content of my first article.  This is content of my first article.  This is content of my first article.
-          This is content of my first article.  This is content of my first article.
-          </p>
-          <p>
-            This is content of my first article.  This is content of my first article.  This is content of my first article.
-              This is content of my first article.  This is content of my first article.
-              </p>`
-              },
-              'article-two' : {
-                title : 'Article two i kanth',
-                heading : 'Article two',
-                date : 'sep11,2017',
-                content : 
-                `<p>
-                This is content of my second article.
-                </p>`
-              },
-              'article-three' : {
-                title : 'Article three i kanth',
-                heading : 'Article three',
-                date : 'sep9,2017',
-                content : 
-                `<p>
-                This is content of my third article.
-                </p>`
-              }
-};
 
-                function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate= `
-<html>
-<head>
-    <title>
-        ${title}
-    </title>
-         <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class = "container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-            <h3>
-            ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-         ${content}
-        </div>
-    </div>
-    </body>
-    </html>
-    `;
-    return htmlTemplate;
-    }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-function hash (input,salt) {
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
-}
-    //how to we create a hash
+
+
+
+var Articles={
+'article-one':{
+    title:'Article-One | Revanth Rajendran',
+    heading:'Article-one',
+    date: '10-Aug-17',
+    content:`	<p>
+        					This is the content for my first article. 
+        		</p>
+        	 <input type="text" id="name" placeholder="comment"></input>
+          <input type="submit" value="submit" id="submit_btn"></input>
+`},
+'article-two':{
+    title:'Article-Two | Revanth Rajendran',
+    heading:'Article-Two',
+    date: '10-Aug-17',
+    content:`	<p>
+        					This is the content for my second article. 
+        		</p>
+        			 <input type="text" id="name" placeholder="comment"></input>
+          <input type="submit" value="submit" id="submit_btn"></input>
+`},
+'article-three':{
+    title:'Article-Three | Revanth Rajendran',
+    heading:'Article-Three',
+    date: '20-Aug-17',
+    content:`	<p>
+        					This is the content for my third article. 
+        		</p>
+        			 <input type="text" id="name" placeholder="comment"></input>
+          <input type="submit" value="submit" id="submit_btn"></input>
+`},
+};
+
+function createHtml(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
     
-    app.get('/hash/:input', function(req,res) {
-        var hashedString = hash(req.params.input, 'this-is-some-random-string');
-        res.send(hashedString);
-    });
-var pool=new Pool(config);
-app.get('/test-db', function (req,res) {
-    //make a select request
-    //return a response with results
-pool.query('SELECT * FROM test',function (req,res) {
-    if (err){
-        res.status(500).send(err.toString());
-        } else{
-            res.send(JSON.stringify(result));
-        }
-});
-});
-var counter = 0;
-app.get('/counter',function(req,res) {
+    var HtmlTemplate=`
+                    <html>
+                    <head>
+                    	<title>
+                    	    ${title}
+                    	</title>
+                    	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    	<link href="/ui/style.css" rel="stylesheet" />
+                    </head>
+                        
+                    <body>
+                        		<div class="container">
+                            		<div>
+                            			<a href="/">HOME</a>
+
+                            		</div>
+                            		   <hr/>
+                            			    <h3> 
+                            		    	${heading}
+                            		    	</h3>
+                                			<div>
+                                			${date}
+                                			</div>
+                                			<div>
+                            		    	${content}
+                            		    	</div>
+                        			</div>
+                    </body>
+                    </html>
+                    `;
+                    return HtmlTemplate;
+}
+
+var counter=0;
+app.get('/counter',function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
 
-var names = [];
-app.get('/submit-name/:name',function (req,res){ //URL:/submit-name?name=xxxxx
-    //get the name from request
-var name = req.query.name;
-names.push(name);//concatenate to overall list of names
-//json:javascript object notation
-res.send(JSON.stringify(names));
+var names=[];
+app.get('/submitname',function(req,res){
+    var name=req.query.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName',function (req,res) {
-    var articleName = req.query.articleName;
-    res.send(createTemplate(articles[articleName]));
-    });
- app.get('/ui/style.css', function (req, res) {
+var pool = new Pool(config);
+app.get("/test.db",function(req,res){
+//make a select request 
+//return a response with the results
+pool.query('SELECT * FROM test', function (err,result){
+    if(err){
+        res.status(500).send(err.toString());    
+    }else{
+        res.send(JSON.stringify(result));
+    }
+});
+});
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+    res.send(createHtml(Articles[articleName]));
+});
+
+
+
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-    app.get('/ui/main.js', function (req, res) {
+
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-    });
+});
+
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
+
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
