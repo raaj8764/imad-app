98c1f9a48e11eb16d18cb0e9e109be562442db10
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-28 18:26:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.get('/article/:articleName', function (req, res) {
            res.status(404).send('article not found');
        }else{
            var articleData=result.rows[0];
-            res.send(createHtml(Articles[articleName]));
+            res.send(createHtml(articleData));
        }
        
         
