f9dca6edc692727c404095a19ebf1ac0c359d989
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 17:07:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool =require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser= require('body-parser');
 var config ={
     host:"db.imad.hasura-app.io",
     user:"revanthr47",
@@ -12,7 +13,7 @@ var config ={
 };
 var app = express();
 app.use(morgan('combined'));
-
+app.user(bodyParser.JSON());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -28,6 +29,27 @@ app.get('/hash/:input', function (req,res){
     var hashedString= hash(req.params.input,'revanthr47');
     res.send(hashedString);
 });
+
+
+app.post('/create-user',function(req,res){
+   
+   var username=req.body.username;
+   var password=req.body.password;
+   
+   
+   
+   var salt=crypto.getRandomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',function(req,res){
+        if(err){
+        res.status(500).send(err.toString());    
+    }else{
+        
+        res.send('user sucessfully creaated:' +username);
+    }
+   });
+});
+
 
 function createTemplate (data){
     var title=data.title;
