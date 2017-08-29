73fdfcbb9bf0c1362757af58529af38eecd3a361
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 17:15:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post('/create-user',function(req,res){
    
    
    
-   var salt=crypto.getRandomBytes(128).toString('hex');
+   var salt=crypto.randomBytes(128).toString('hex');
    var dbString=hash(password,salt);
    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',function(req,res){
         if(err){
