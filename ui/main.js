7c7062b66e38668fdcccd09d2401c6b60292de70
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 21:03:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,44 +1,24 @@
-var button = document.getElementById('counter');
-//var counter = 0;
-document.getElementById("button").onclick = function (){
-    
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE){
-        if (request.status === 200){
-           var counter = request.responseText;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
-}
-},
-request.open('GET', 'http://revanthr47.imad.hasura-app.io/counter', true);
-request.send(null);
-};
 
-
-var nameinput = document.getElementById('name');
-var name = nameinput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
      var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE){
         if (request.status === 200){
-           var names = request.responseText;
-   names = JSON.parse(names);
-   var list = '';
-   for(var i=0; i< names.length; i++){
-       
-       list += '<li>' + names[i] + '</li>';
-   }
-    
-    var ul =document.getElementById('namelist');
-    ul.innerHTML = list;
-}
-}
+           console.log('user logged in');
+           alert('user logged in');
+        }else if(request.status === 403){
+            alert('username/password incorrect');
+        }else if(request.status === 500){
+             alert('something went wrong in server');
+        }
+    }
 };
-
-request.open('GET', 'http://revanthr47.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', 'http://revanthr47.imad.hasura-app.io/login', true);
+request.setRequestHeader('Content-Type','application/json');
+request.send(JSON.stringify({username:username,password:password}));
 };
