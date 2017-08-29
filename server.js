519177f99a497922891c1f066f4b1fd0791f73c4
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 21:28:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/login', function (req, res){
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString){
                    //set the session
-                   req.session.auth ={userId: result.rows[0].id;
+                   req.session.auth ={userId: result.rows[0].id};
                    
                    
                     res.send('login succesfull');
