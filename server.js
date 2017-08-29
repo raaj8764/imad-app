25f96ca4de1741175bb29b7a07088b5a7905b00c
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 20:07:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login',function(req,res){
         if(err){
         res.status(500).send(err.toString());    
         }else{
-            if(result.rows.lenght===0){
+            if(result.rows.length===0){
                 res.send(403).send('user/password incorrect');
             }else{
                 //match the password
