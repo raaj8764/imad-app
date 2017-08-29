bf440d6d26241c91e920f89377caa3099a67c28d
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 21:27:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool =require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser= require('body-parser');
+var session=require('express-session');
 var config ={
     host:"db.imad.hasura-app.io",
     user:"revanthr47",
@@ -15,6 +16,9 @@ var config ={
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+   secret: "someRandomSecretValue", cookies: {maxAge: 1000*60*60*24*30}
+}));
 
 
 
@@ -69,11 +73,12 @@ app.post('/login', function (req, res){
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password, salt);
                 if(hashedPassword === dbString){
+                   //set the session
+                   req.session.auth ={userId: result.rows[0].id;
+                   
+                   
                     res.send('login succesfull');
-                    
-                    //set a session 
-                    
-                    
+                   
                     }else{
                     res.send(403).send('username/password incorrect');
                     }
@@ -82,6 +87,13 @@ app.post('/login', function (req, res){
    });
 });
 
+app.get('/check-login', function(req,res){
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('you are logged in' + req.session.auth.userId.toString());
+    }else{
+        res.send('you are not logged in ');
+    }
+});
 
 function createTemplate (data){
     var title=data.title;
