ea193b97a187b456c7e155e0e8dcd8e5a0857c7b
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 17:53:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/hash/:input', function (req,res){
 });
 
 
-app.post('/create-user',function(req,res){
+app.post('/create_user',function(req,res){
    
    var username=req.body.username;
    var password=req.body.password;
