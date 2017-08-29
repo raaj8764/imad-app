1839534e2b6893abe3fa68fdbc17ace772f833cc
raaj8764 <30666986+raaj8764@users.noreply.github.com>
2017-08-29 17:09:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var config ={
 };
 var app = express();
 app.use(morgan('combined'));
-app.user(bodyParser.JSON());
+app.use(bodyParser.JSON());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
